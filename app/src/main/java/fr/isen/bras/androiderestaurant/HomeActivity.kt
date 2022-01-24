f24e2a6b9d438b5c651b213a66a787503eca79c1
ocebr <oceaneb13@hotmail.fr>
2022-01-24 16:39:52 +0100

    toast

--- app/src/main/java/fr/isen/bras/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/bras/androiderestaurant/HomeActivity.kt
@@ -2,10 +2,24 @@ package fr.isen.bras.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+
+        val tvStarter = findViewById<TextView>(R.id.starters)
+        // set on-click listener
+        tvStarter.setOnClickListener {
+            // your code to perform when the user clicks on the TextView
+            Toast.makeText(this@HomeActivity, "You clicked on Starters.", Toast.LENGTH_LONG).show()
+            //Log.w("home activity", "je suis passé par ici")
+        }
+
     }
+
 }
