71fd8209e1ac96df5736c40a776a0dc84524007f
ocebr <oceaneb13@hotmail.fr>
2022-01-24 17:55:32 +0100

    change activity for all category

--- app/src/main/java/fr/isen/bras/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/bras/androiderestaurant/HomeActivity.kt
@@ -14,23 +14,48 @@ class HomeActivity : AppCompatActivity() {
 
 
         val tvStarter = findViewById<TextView>(R.id.starters)
+        val tvMainCourse = findViewById<TextView>(R.id.Maincourse)
+        val tvDeserts = findViewById<TextView>(R.id.Deserts)
+
+
         // set on-click listener
         tvStarter.setOnClickListener {
+            val str = tvStarter.getText()
             // your code to perform when the user clicks on the TextView
             Toast.makeText(this@HomeActivity, "You clicked on Starters.", Toast.LENGTH_LONG).show()
             //Log.w("home activity", "je suis passé par ici")
-        }
-
-        val changeActivity = findViewById<TextView>(R.id.starters)
-        val str = changeActivity.getText()
-
-        val monIntent : Intent =  Intent(this,SelectedCategoryActivity::class.java)
-        monIntent.putExtra("selectedCategory", str)
-
-
-        changeActivity.setOnClickListener {
+            val monIntent : Intent =  Intent(this,SelectedCategoryActivity::class.java)
+            monIntent.putExtra("selectedCategory", str)
             startActivity(monIntent)
         }
+        tvMainCourse.setOnClickListener {
+            val str = tvMainCourse.getText()
+            // your code to perform when the user clicks on the TextView
+            Toast.makeText(this@HomeActivity, "You clicked on Main courses.", Toast.LENGTH_LONG).show()
+            //Log.w("home activity", "je suis passé par ici")
+            val monIntent : Intent =  Intent(this,SelectedCategoryActivity::class.java)
+            monIntent.putExtra("selectedCategory", str)
+            startActivity(monIntent)
+        }
+        tvDeserts.setOnClickListener {
+            val str = tvDeserts.getText()
+            // your code to perform when the user clicks on the TextView
+            Toast.makeText(this@HomeActivity, "You clicked on Desert.", Toast.LENGTH_LONG).show()
+            //Log.w("home activity", "je suis passé par ici")
+            val monIntent : Intent =  Intent(this,SelectedCategoryActivity::class.java)
+            monIntent.putExtra("selectedCategory", str)
+            startActivity(monIntent)
+
+
+        }
+
+
+
+
+
+
+
+
 
     }
 
