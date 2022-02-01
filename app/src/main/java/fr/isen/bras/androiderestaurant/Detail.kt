76df81bbbf1897cc93f69a1e90a88d9b0f116b3a
ocebr <oceaneb13@hotmail.fr>
2022-02-01 13:21:03 +0100

    viewpager for carrousel buttons to increase or decrease quantity and total price display

--- app/src/main/java/fr/isen/bras/androiderestaurant/Detail.kt
+++ app/src/main/java/fr/isen/bras/androiderestaurant/Detail.kt
@@ -2,11 +2,14 @@ package fr.isen.bras.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
-import com.squareup.picasso.Picasso
 import fr.isen.bras.androiderestaurant.model.DishModel
+
+
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class Detail : AppCompatActivity() {
 
@@ -14,45 +17,71 @@ class Detail : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
-        var item: String? = ""
-
-
         val itemDish= intent.getSerializableExtra("itemDish") as DishModel
 
+        var quantity : Int=1
 
 
         val detail_title = findViewById<TextView>(R.id.detailtitle)
-        detail_title.setText(itemDish.name_fr)
-        val detail_image = findViewById<ImageView>(R.id.detailImage)
-        if(itemDish.images[0]!="") {
-            Picasso.get()
-                .load(itemDish.images[0])
-                .error(R.drawable.pizza)
-                .into(detail_image)
-
-
-
-        }
-        else{
-            detail_image.setImageResource(R.drawable.pizza)
-        }
-
-        val detail_price = findViewById<TextView>(R.id.detailprice)
-        detail_price.setText(itemDish.prices[0].price + "€")
-
+        var buttonmoins =findViewById<FloatingActionButton>(R.id.moins)
+        var viewPager = findViewById<ViewPager>(R.id.viewPager)
         val detail_text = findViewById<TextView>(R.id.detailtext)
-
-        for (i in itemDish.ingredients)
-            detail_text.append(i.name_fr + " ")
-
-
-
-
-
-
-
+        var quantitydiplay =findViewById<TextView>(R.id.quantity)
+        val pricedisplay = findViewById<TextView>(R.id.detailprice)
+        var buttonplus =findViewById<FloatingActionButton>(R.id.plus)
         val back =findViewById<Button>(R.id.detailback)
-        back.setOnClickListener {
+
+        //carrousel image
+        var imgs: List <String> = itemDish.images
+        var adapter = ViewPagerAdapter(this, imgs)
+        viewPager.adapter =adapter
+
+
+
+        //title display
+        detail_title.setText(itemDish.name_fr)
+
+
+        //ingredients display
+        for (i in itemDish.ingredients) detail_text.append(i.name_fr + " ")
+
+        //price display
+        pricedisplay.text = "Total   "+itemDish.prices[0].price+ "€"
+
+       //quantity display
+        quantitydiplay.text="$quantity"
+
+
+        //click listener
+
+        buttonplus.setOnClickListener{
+            quantity++
+
+            val totalprice = (itemDish.prices[0].price.toInt())*quantity
+            pricedisplay.text = "Total   "+"$totalprice"+ "€"
+            quantitydiplay.text="$quantity"
+
+
+        }
+
+
+        buttonmoins.setOnClickListener(){
+            if (quantity>1) quantity --
+            else quantity =1
+
+            val totalprice = (itemDish.prices[0].price.toInt())*quantity
+            pricedisplay.text="Total   "+"$totalprice"+ "€"
+            quantitydiplay.text="$quantity"
+
+        }
+
+        quantitydiplay.setOnClickListener(){
+
+        }
+
+
+        //back bbutton
+       back.setOnClickListener {
             finish()
         }
 
---
+++ app/src/main/java/fr/isen/bras/androiderestaurant/ViewPagerAdapter.kt
@@ -0,0 +1,54 @@
+package fr.isen.bras.androiderestaurant
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.viewpager.widget.PagerAdapter
+import com.squareup.picasso.Picasso
+import java.util.*
+
+
+internal class ViewPagerAdapter(var context: Context, var images: List<String>) : PagerAdapter() {
+
+
+    lateinit var layoutInflater: LayoutInflater
+
+    override fun getCount(): Int {
+        return images.size
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view.equals( `object`)    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+
+        layoutInflater= LayoutInflater.from(context)
+        var view = layoutInflater.inflate(R.layout.image_ressource_file, container,false)
+        val img : ImageView =view.findViewById<ImageView>(R.id.imageView)
+
+        if(images[position]!="") {
+            Picasso.get()
+                .load(images[position])
+                .error(R.drawable.pizza)
+                .into(img)
+
+
+
+        }
+        else{
+            img.setImageResource(R.drawable.pizza)
+        }
+        container.addView(view,0)
+        return view
+
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View)
+    }
+
+
+}
